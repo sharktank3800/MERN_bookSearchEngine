8edaa146d7b2aad837ab797d8777a809702931b7
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-18 21:37:18 -0500

    implemented the apollo server applied it to express server as middleWare and set up resolvers,  created schema folder have typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,21 +1,47 @@
 const express = require('express');
+
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// importing apollo server
+const {ApolloServer} = require("@apollo/server");
+// import expressMiddleware to integrate apollo server with express , allows to mount the apollo Server middleware in an express app
+const {expressMiddleware} = require("@apollo/server/express4");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const {typeDefs, resolvers} = require("./schema");
+const {authenticate} = require("./utils/auth");
+
+// create an instance of ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+// function to start the server 
+async function startServer(){
+  await server.start();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/graphql", expressMiddleware(server, {
+  context: authenticate
+}))
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
 });
+}
+
+startServer();
