1ba76c5ae1dcb89eebdd2eabefd8c1f6908898a6
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-19 22:25:00 -0500

    finished mutations executing all the mutations corresponding to resolvers and typeDefs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,3 +10,52 @@ mutation loginUser($email: String!, $password: String!){
         }
     }
 }`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username: $username, email: $email, password: $password){
+        token
+        user{
+            _id
+            username
+            email
+        }
+    }
+}`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: BookInput){
+    saveBook(input: $input){
+        _id
+        username
+        bookCount
+        saveBook{
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}`;
+
+
+export const DELETE_BOOK = gql`
+mutation deleteBook($bookId: String!){
+    deleteBook(bookId: $bookId){
+        _id
+        username
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}`;
+
+
