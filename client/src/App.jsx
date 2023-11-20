2c45f993db9673f272dcc0ed6472092c5556534b
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-19 23:36:02 -0500

    created an apollo provider making every request working with apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client"
 
 import Navbar from './components/Navbar';
 
+// creating http link
+const httpLink = createHttpLink({
+  uri: "http://localhost: 3001/graphql"
+})
+
+// create an Apollo client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache
+})
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <>
       <Navbar />
       <Outlet />
     </>
+    </ApolloProvider>
   );
 }
 
